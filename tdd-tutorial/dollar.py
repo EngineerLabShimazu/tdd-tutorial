2193b7b4f3781ae7e6ae82ed6d652b0603a84ccf
GenkiShimazu <shimazu.g.r@gmail.com>
2019-05-24 15:27:53 +0900

    Dollar timesの設計の問題点を解消

--- tdd-tutorial/dollar.py
+++ tdd-tutorial/dollar.py
@@ -1,8 +1,6 @@
 class Dollar:
     def __init__(self, amount):
         self.amount = amount
-        pass
 
     def times(self, multiplier):
-        self.amount *= multiplier
-        pass
+        return Dollar(self.amount * multiplier)
