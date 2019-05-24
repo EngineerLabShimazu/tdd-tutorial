18d53d66d0466fbebf23e5ead8e61a287e3f94f5
GenkiShimazu <shimazu.g.r@gmail.com>
2019-05-24 16:56:40 +0900

    実践のことを考えて、TODOリストはコードに記載するようにした。

--- tests/test_money.py
+++ tests/test_money.py
@@ -3,6 +3,7 @@ from dollar import Dollar
 
 
 class DollarTest(unittest.TestCase):
+    # TODO $5 + 10CHF = $10(レートが2:1の場合)
     def test_multiplication(self):
         five = Dollar(5)
         product = five.times(2)
@@ -11,6 +12,8 @@ class DollarTest(unittest.TestCase):
         self.assertEqual(15, product.amount)
 
     def test_equality(self):
+        # TODO nullとの等価性比較
+        # TODO 他のオブジェクトとの等価性比較
         self.assertTrue(Dollar(5).equals(Dollar(5)))
         self.assertFalse(Dollar(5).equals(Dollar(6)))
 
